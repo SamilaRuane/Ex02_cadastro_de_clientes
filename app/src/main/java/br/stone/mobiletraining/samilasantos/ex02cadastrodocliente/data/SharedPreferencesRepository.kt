9e9397b11a7b1ab3e8c184662374acdb110934b8
Samila Ruane <samilaruane@gmail.com>
2018-07-20 19:57:21 -0300

    Finishes database implementation.

--- app/src/main/java/br/stone/mobiletraining/samilasantos/ex02cadastrodocliente/data/SharedPreferencesRepository.kt
+++ app/src/main/java/br/stone/mobiletraining/samilasantos/ex02cadastrodocliente/data/SharedPreferencesRepository.kt
@@ -1,54 +1,111 @@
 package br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.data
 
 import android.content.Context
+import br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.data.common.Mapper
 import br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.domain.Entrepreneur
 import br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.domain.Repository
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import org.json.JSONObject
-import java.util.*
 
 class SharedPreferencesRepository(val context: Context) : Repository {
 
-    private val sharedPreferences = context.getSharedPreferences(KEYS.SHARED_PREFERENCE_KEY.name, Context.MODE_PRIVATE)
+    private val sharedPreferences = context.getSharedPreferences(KEYS.SHARED_PREFERENCE_KEY.key, Context.MODE_PRIVATE)
 
     override fun create(entity: Entrepreneur): Boolean {
+        delete(entity)
         val editor = sharedPreferences.edit()
+        val idNumber = (sharedPreferences.getLong(KEYS.LAST_ID_NUMBER.key, 0)) + 1
         val entrepreneurInfoMap = hashMapOf(
-                KEYS.ENTREPRENEUR_FULL_NAME.name to entity.fullName,
-                KEYS.ENTREPRENEUR_EMAIL.name to entity.email,
-                KEYS.ENTREPRENEUR_PHONE.name to entity.phone.toString(),
-                KEYS.ENTREPRENEUR_TRADE_NAME.name to entity.tradeName,
-                KEYS.ENTREPRENEUR_BIRTH_DATE.name to entity.birthDate.time.toString(),
-                KEYS.ENTREPRENEUR_INDIVIDUAL.name to if (entity.individualEntrepreneur) "Sim" else "Não")
+                KEYS.ENTREPRENEUR_ID.key to idNumber.toString(),
+                KEYS.ENTREPRENEUR_FULL_NAME.key to entity.fullName,
+                KEYS.ENTREPRENEUR_EMAIL.key to entity.email,
+                KEYS.ENTREPRENEUR_PHONE.key to entity.phone.toString(),
+                KEYS.ENTREPRENEUR_TRADE_NAME.key to entity.tradeName,
+                KEYS.ENTREPRENEUR_BIRTH_DATE.key to entity.birthDate.time.toString(),
+                KEYS.ENTREPRENEUR_INDIVIDUAL.key to if (entity.individualEntrepreneur) "Sim" else "Não")
 
         val json = JSONObject(entrepreneurInfoMap)
-        editor.putString(KEYS.ENTREPRENEUR_DATA.name, json.toString()).apply()
+        val existentData = sharedPreferences.getString(KEYS.ENTREPRENEUR_DATA.key, "")
+        editor.putString(KEYS.ENTREPRENEUR_DATA.key,
+                if (!existentData.isEmpty()) existentData + "," + json.toString()
+                else json.toString()).apply()
+        editor.putLong(KEYS.LAST_ID_NUMBER.key, idNumber).apply()
         return true
     }
 
-    override fun delete(entity: Entrepreneur): Boolean {
+    private fun create(entity: EntrepreneurData): Boolean {
+        val editor = sharedPreferences.edit()
+        val idNumber = (sharedPreferences.getLong(KEYS.LAST_ID_NUMBER.key, 0)) + 1
+        val entrepreneurInfoMap = hashMapOf(
+                KEYS.ENTREPRENEUR_ID.key to idNumber.toString(),
+                KEYS.ENTREPRENEUR_FULL_NAME.key to entity.fullName,
+                KEYS.ENTREPRENEUR_EMAIL.key to entity.email,
+                KEYS.ENTREPRENEUR_PHONE.key to entity.phone.toString(),
+                KEYS.ENTREPRENEUR_TRADE_NAME.key to entity.tradeName,
+                KEYS.ENTREPRENEUR_BIRTH_DATE.key to entity.birthDate.toString(),
+                KEYS.ENTREPRENEUR_INDIVIDUAL.key to if (entity.individualEntrepreneur) "Sim" else "Não")
+
+        val json = JSONObject(entrepreneurInfoMap)
+        val existentData = sharedPreferences.getString(KEYS.ENTREPRENEUR_DATA.key, "")
+        editor.putString(KEYS.ENTREPRENEUR_DATA.key,
+                if (!existentData.isEmpty()) existentData + "," + json.toString()
+                else json.toString()).apply()
+        editor.putLong(KEYS.LAST_ID_NUMBER.key, idNumber).apply()
         return true
     }
 
-    override fun findAll(): List<Entrepreneur> = ArrayList()
+    override fun delete(entity: Entrepreneur): Boolean = try {
+        val data = (getData() as ArrayList)
+        val targetObject = data.single { it.fullName == entity.fullName }
+        data.remove(targetObject)
 
+        sharedPreferences.edit().putString(KEYS.ENTREPRENEUR_DATA.key, "").apply()
+        for (entrepreneurData in data) {
+            create(entrepreneurData)
+        }
 
+        true
+    } catch (e: NoSuchElementException) {
+        false
+    }
+
+    override fun findAll(): List<Entrepreneur> {
+        val entrepreneurList = getData()
+        val mappedList = ArrayList<Entrepreneur>()
+        for (data in entrepreneurList) {
+            mappedList.add(Mapper.toEntrepreneur(data))
+        }
+        return mappedList
+    }
+
+    private fun getData(): List<EntrepreneurData> {
+        val databaseData = "{ \"entrepreneurData\": [ " + sharedPreferences.getString(KEYS.ENTREPRENEUR_DATA.key, "") + "]}"
+        return Gson().fromJson(databaseData, EntrepreneurList::class.java).entrepreneurData
+    }
 }
 
+data class EntrepreneurList(val entrepreneurData: List<EntrepreneurData>)
 data class EntrepreneurData(val id: Long,
+                            @SerializedName("full_name")
                             val fullName: String,
                             val email: String,
                             val phone: Long,
+                            @SerializedName("trade_name")
                             val tradeName: String,
-                            val birthDate: Date,
+                            val birthDate: Long,
+                            @SerializedName("individual_entrepreneur")
                             val individualEntrepreneur: Boolean)
 
 enum class KEYS(val key: String) {
     SHARED_PREFERENCE_KEY("entrepreneur_db"),
+    ENTREPRENEUR_DATA("entrepreneur_data"),
+    LAST_ID_NUMBER("last_id_number"),
+    ENTREPRENEUR_ID("id"),
     ENTREPRENEUR_FULL_NAME("full_name"),
     ENTREPRENEUR_EMAIL("email"),
     ENTREPRENEUR_PHONE("phone"),
     ENTREPRENEUR_TRADE_NAME("trade_name"),
     ENTREPRENEUR_BIRTH_DATE("birth_date"),
-    ENTREPRENEUR_INDIVIDUAL("individual_entrepreneur"),
-    ENTREPRENEUR_DATA("entrepreneur_data")
+    ENTREPRENEUR_INDIVIDUAL("individual_entrepreneur")
 }
--- app/src/main/java/br/stone/mobiletraining/samilasantos/ex02cadastrodocliente/ui/App.kt
+++ app/src/main/java/br/stone/mobiletraining/samilasantos/ex02cadastrodocliente/ui/App.kt
@@ -1,6 +1,8 @@
 package br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.ui
 
 import android.app.Application
+import br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.data.SharedPreferencesRepository
+import br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.ui.di.GraphBuilder
 import br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.ui.di.GraphConfigurator
 import br.stone.mobiletraining.samilasantos.ex02cadastrodocliente.ui.di.Mode
 
@@ -9,5 +11,10 @@ class App : Application(){
 
     init {
         injector.mode = Mode.App
+    }
+
+    override fun onCreate() {
+        injector.mode = Mode.Test (GraphBuilder.builder().override().repository(SharedPreferencesRepository(this)).build()!!)
+        super.onCreate()
     }
 }
